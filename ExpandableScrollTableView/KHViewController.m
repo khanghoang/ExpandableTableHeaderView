307833eeb7dc50931212878bb0914a25f9a8b58a
khanghoang <hoangtrieukhang@gmail.com>
2014-07-28 16:20:02 +0700

    Initial commit

--- ExpandableScrollTableView/KHViewController.m
+++ ExpandableScrollTableView/KHViewController.m
@@ -8,7 +8,10 @@
 
 #import "KHViewController.h"
 
-@interface KHViewController ()
+@interface KHViewController () <UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UIView *expandableScrollHeader;
 
 @end
 
@@ -18,12 +21,46 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
+    UIEdgeInsets edge = UIEdgeInsetsMake(self.expandableScrollHeader.frame.size.height, 0, 0, 0);
+
+    [self.tableView setContentInset:edge];
+    [self.tableView setScrollIndicatorInsets:edge];
+
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 100;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"BasicCell" forIndexPath:indexPath];
+    cell.textLabel.text = [NSString stringWithFormat:@"%d", indexPath.row];
+    return cell;
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    CGFloat contentInsetY = self.tableView.contentOffset.y;
+    if(contentInsetY < 0) {
+        contentInsetY = ABS(contentInsetY);
+    }
+    else {
+        contentInsetY = 0;
+    }
+
+    CGFloat topViewHeight = contentInsetY;
+
+    CGRect newFrame = self.expandableScrollHeader.frame;
+    newFrame.size.height = topViewHeight;
+
+    self.expandableScrollHeader.frame = newFrame;
 }
 
 @end
